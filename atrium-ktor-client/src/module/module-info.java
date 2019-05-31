1d35c109dc1f0f36e2587b7e9d71a7665127c45f
Robert Stoll <robstoll@tegonal.com>
2019-05-31 10:40:39 +0200

    apply module-info plugin, add missing require statements

--- atrium-ktor-client/src/module/module-info.java
+++ atrium-ktor-client/src/module/module-info.java
@@ -1,5 +1,8 @@
 module com.tegonal.atrium.ktor.client {
     requires kotlin.stdlib;
+    requires ktor.client.core.jvm;
+    requires ktor.http.jvm;
+    requires ch.tutteli.atrium.bundle.cc.en_GB.robstoll;
 
     exports com.tegonal.atrium.ktor.client;
 }
