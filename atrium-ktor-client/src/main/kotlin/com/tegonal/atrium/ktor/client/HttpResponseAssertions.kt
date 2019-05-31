e2c0cb248891bcbf3840945d7daaf3514d5d9144
Robert Stoll <robstoll@tegonal.com>
2019-05-31 09:24:02 +0200

    document functions

--- atrium-ktor-client/src/main/kotlin/com/tegonal/atrium/ktor/client/HttpResponseAssertions.kt
+++ atrium-ktor-client/src/main/kotlin/com/tegonal/atrium/ktor/client/HttpResponseAssertions.kt
@@ -6,7 +6,18 @@ import ch.tutteli.atrium.creating.Assert
 import io.ktor.client.response.HttpResponse
 import io.ktor.http.HttpStatusCode
 
-val <T: HttpResponse> Assert<T>.status get() = property(io.ktor.client.response.HttpResponse::status)
+/**
+ * Shortcut for `property(HttpResponse::status)`.
+ */
+val <T : HttpResponse> Assert<T>.status get() : Assert<HttpStatusCode> = property(HttpResponse::status)
 
-fun <T: HttpResponse> Assert<T>.hasStatus(httpStatusCode: HttpStatusCode): Assert<HttpResponse> =
+/**
+ * Makes the assertion that the status of the [HttpResponse] is the given [httpStatusCode].
+ *
+ * Is a shortcut for `property(HttpResponse::status).toBe(httpStatusCode)`
+ *
+ * Notice that an equality check is applied and that [HttpStatusCode.equals] is overridden and only compares the
+ * [HttpStatusCode.value].
+ */
+fun <T : HttpResponse> Assert<T>.hasStatus(httpStatusCode: HttpStatusCode): Assert<HttpResponse> =
     apply { status.toBe(httpStatusCode) }
